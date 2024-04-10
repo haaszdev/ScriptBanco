eeb122b6abf9cdedbbb484f61efe466e02d6925d
haaszdev <joao.haas@alunos.bpkedu.com.br>
2024-04-09 23:47:02 -0300

    adicionando o script de respostas

--- scriptRespostas.py
+++ scriptRespostas.py
@@ -2,17 +2,14 @@ from faker import Faker
 import random
 import mysql.connector
 
-# fake = Faker('pt_BR')
+fake = Faker(locale='pt_BR')
 
-fake = Faker()
-
-# Valores aleatórios para setor_id, porte_id e eixo_id
+# Valores aleatórios para Empresa_id, Perguntas_id e Formulario_id
 def gerar_valores_aleatorios():
-    setor_id = random.randint(1, 6)
-    porte_id = random.randint(1, 4)
-    eixo_id = random.randint(1, 3)  
-    return setor_id, porte_id, eixo_id
-
+    Empresa_id = random.randint(1, 6)
+    Perguntas_id = random.randint(1, 4)
+    Formulario_id = random.randint(1, 3)
+    return Empresa_id, Perguntas_id, Formulario_id
 
 conn_mysql = mysql.connector.connect(
     host='localhost',
@@ -28,11 +25,11 @@ contador_commits = 0
 
 while True:
     # Gerar dados fictícios
-    titulo = fake.text(max_nb_chars=45)
-    descricao = fake.text(max_nb_chars=200)  
-    setor_id, porte_id, eixo_id = gerar_valores_aleatorios()
+    resposta = fake.text(max_nb_chars=45)
+    url_arquivo = fake.file_path(depth=2)
+    Empresa_id, Perguntas_id, Formulario_id = gerar_valores_aleatorios()
 
-    cursor.execute("INSERT INTO perguntas (titulo, descricao, setor_id, porte_id, eixo_id) VALUES (%s, %s, %s, %s, %s)", (titulo, descricao, setor_id, porte_id, eixo_id))
+    cursor.execute("INSERT INTO respostas (resposta, url_arquivo, Empresa_id, Perguntas_id, Formulario_id) VALUES (%s, %s, %s, %s, %s)", (resposta, url_arquivo, Empresa_id, Perguntas_id, Formulario_id))
     contador_linhas += 1
 
     # Fazer commit a cada 15 linhas inseridas
@@ -40,7 +37,6 @@ while True:
         conn_mysql.commit()
         contador_commits += 1
         print(f"Commit {contador_commits} realizado. Linhas inseridas até agora: {contador_linhas}")
-
 
 cursor.close()
 conn_mysql.close()
